2f7f5a323f7822a043a3081c1c912f0cb5939d50
Michael Carlino <110254337+Linosway24@users.noreply.github.com>
2026-07-03 12:56:56 -0400

    Initial Commit

---
+++ SplatCoachCapture/SplatCoachCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplatCoachCaptureApp.swift
+//  SplatCoachCapture
+//
+//  Created by Michael Carlino on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SplatCoachCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
